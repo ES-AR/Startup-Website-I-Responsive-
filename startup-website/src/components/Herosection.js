b383f20c1d1c8e575209c08b3579ab0dbda5a050
es~ar <sadiqridwan347@gmail.com>
2025-04-06 14:12:16 +0100

    hero section

--- startup-website/src/components/Herosection.js
+++ startup-website/src/components/Herosection.js
@@ -1,7 +1,7 @@
 import './Herosection.css'
 import Group from '../assets/Group.png';
 import { useState } from "react";
-import { motion } from "framer-motion";
+
 const Herosection = () => {
     const [buttonColor, setButtonColor] = useState("white");
     const handleClick = () => {
@@ -11,15 +11,6 @@ const Herosection = () => {
         <section>
             <div className="herosec">
                 <div className="groupdiv">
-                   <motion.img 
-                        src={Group} 
-                        className='group' 
-                        alt="Group pic"
-                        initial={{ opacity: 0, y: -50 }} // Starts faded & moves down
-                        animate={{ opacity: 1, y: 0 }}  // Fades in & moves into position
-                        transition={{ duration: 1, ease: "easeOut" }} // Smooth animation
-                        whileHover={{ scale: 1.1 }} // Slight zoom on hover
-                    />
                 </div>
                 <div className="groupseconddiv">
                     <h5 className="welcome">W E L C O M E</h5>
