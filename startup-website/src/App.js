702d723d6009febd925e60711ee6373e7f04bf49
es~ar <sadiqridwan347@gmail.com>
2025-04-06 13:26:35 +0100

    assets added

--- startup-website/src/App.js
+++ startup-website/src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import Navbar from "./components/Navbar";  // Import the Navbar
+import Home from "./pages/Home";
+import Portfolio from "./pages/Portfolio";
+import Services from "./pages/Services";
+import Contactus from "./pages/Contactus";
 
 function App() {
   return (
