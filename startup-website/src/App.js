fc9923b5b8cc416a55ab5c8e136ae65c4e066eca
es~ar <sadiqridwan347@gmail.com>
2025-04-07 08:18:23 +0100

    services.js added to App.js

--- startup-website/src/App.js
+++ startup-website/src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";  // Import the Navbar
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
 import Contactus from "./pages/Contactus";
+import Services from "./pages/Services";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/contact" element={<Contactus />} />
+        <Route path="/services" element={<Services />} />
       </Routes>
     </Router>
   );
